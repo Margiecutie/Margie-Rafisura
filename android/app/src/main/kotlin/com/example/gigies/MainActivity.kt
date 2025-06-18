261b80da4d63c53795f527ccbb6f9fd55b21513d
Margiecutie <margiecute09@gmail.com>
2025-06-18 09:41:46 +0800

    MArgie Rafisura

---
+++ android/app/src/main/kotlin/com/example/gigies/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gigies
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Expense Tracker'),
+        backgroundColor: Colors.pink[300],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Ay',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.blue[800],
+                fontWeight: FontWeight.w600,
+                  fontFamily: 'Courier New'
+              ),
+            ),
+            Text(
+              'Wow',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.purple[800],
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Times New Roman'
+              ),
+            ),
+            Text(
+              'Ang Galing',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.green[800],
+                fontWeight: FontWeight.w600,
+                  fontFamily: 'Arial'
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  ));
+}
