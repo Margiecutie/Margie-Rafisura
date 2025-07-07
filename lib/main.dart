59d0dc1eb868bee712e5740d8d7c1bd6eeb5268d
margiecutie <margiecute09@gmail.com>
2025-07-07 11:47:46 +0800

    worldtime

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:gigies/pages/dashboard.dart';
 import 'package:gigies/pages/listitems.dart';
 import 'package:gigies/pages/newpet.dart';
+import 'Worldtime/home.dart';
+import 'Worldtime/choose_location.dart';
+import 'Worldtime/loading.dart';
 
 void main() {
   runApp(MaterialApp(
-      routes:{
-        '/' : (context) => listItems(),
-        '/add' : (context) => NewPet(),
-      }
+    debugShowCheckedModeBanner: false,
+    routes: {
+      '/add': (context) => NewPet(),
+
+      //Worldtime Routes
+      '/home': (context) => Home(),
+      '/': (context) => ChooseLocation(),
+      '/load': (context) => Loading(),
+    },
   ));
 }
-
